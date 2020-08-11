d1c6558f0eaf98e5d94762b8872ac78fd3b304eb
Vinson Chen <vchen@zendesk.com>
2020-08-11 11:11:30 -0700

    unpretty

--- test.js
+++ test.js
@@ -1,20 +1,23 @@
 var test = {
-  12312: 123, asdfasdfsadfsadasfasfas: 'sadfasfasdsdf',
+  12312: 123,
+  asdfasdfsadfsadasfasfas: 'sadfasfasdsdf',
   asdfasfasdfasdfsafda: 'asdfasdfsadfsadasfasfas',
 };
 
-var anotherTest = '123123123'
+var anotherTest =
+'123123123';
 
 var test123 = {
-  12312: 123, asdfasdfsadfsadasfasfas: 'sadfasfasdsdf',
-  asdfasfasdfasdfsafda: 'asdfasdfsadfsadasfasfas', 123:1234
+  12312: 123,
+  asdfasdfsadfsadasfasfas: 'sadfasfasdsdf',asdfasfasdfasdfsafda: 'asdfasdfsadfsadasfasfas',
+  123: 1234,
 };
 
 var test1234 = {
-  12312: 123, asdfasdfsadfsadasfasfas: 'sadfasfasdsdf',
-  asdfasfasdfasdfsafda: 'asdfasdfsadfsadasfasfas', 123:1234
+  12312: 123,
+  asdfasdfsadfsadasfasfas: 'sadfasfasdsdf',asdfasfasdfasdfsafda: 'asdfasdfsadfsadasfasfas',  123: 1234,
 };
 
 var aChange = {
 
-}
+};
