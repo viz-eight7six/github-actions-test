5f33a12bfe26420a64c2f4e80ad65b7bd33a6d45
viz-eight7six <viz-eight7six@users.noreply.github.com>
2020-08-11 18:16:06 +0000

    Apply formatting changes

--- test.js
+++ test.js
@@ -4,20 +4,20 @@ var test = {
   asdfasfasdfasdfsafda: 'asdfasdfsadfsadasfasfas',
 };
 
-var anotherTest =
-'123123123';
+var anotherTest = '123123123';
 
 var test123 = {
   12312: 123,
-  asdfasdfsadfsadasfasfas: 'sadfasfasdsdf',asdfasfasdfasdfsafda: 'asdfasdfsadfsadasfasfas',
+  asdfasdfsadfsadasfasfas: 'sadfasfasdsdf',
+  asdfasfasdfasdfsafda: 'asdfasdfsadfsadasfasfas',
   123: 1234,
 };
 
 var test1234 = {
   12312: 123,
-  asdfasdfsadfsadasfasfas: 'sadfasfasdsdf',asdfasfasdfasdfsafda: 'asdfasdfsadfsadasfasfas',  123: 1234,
+  asdfasdfsadfsadasfasfas: 'sadfasfasdsdf',
+  asdfasfasdfasdfsafda: 'asdfasdfsadfsadasfasfas',
+  123: 1234,
 };
 
-var aChange = {
-
-};
+var aChange = {};
